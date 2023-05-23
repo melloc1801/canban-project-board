329a022ebc8497a89896efbce2602b839d24814e
melloc1801 <dmitry.tomahawk@yandex.ru>
2023-05-23 19:13:18 +0500

    implement refresh tokens

--- CB.WebAPI/Controllers/AuthController.cs
+++ CB.WebAPI/Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 using CB.Application.Features.AuthFeature;
 using CB.Application.Features.AuthFeature.Auth.Dtos;
+using CB.Application.Features.AuthFeature.Refresh.Dtos;
 using CB.Application.Features.AuthFeature.Signin.Dtos;
 using CB.Application.Features.UserFeature.CreateUser.Dtos;
 using Microsoft.AspNetCore.Mvc;
@@ -26,5 +27,11 @@ public class AuthController
     public async Task<AuthResponseDto> Signup([FromBody] CreateUserDto createUserDto)
     {
         return await _authService.Signup(createUserDto);
+    }
+
+    [HttpPost("refresh")]
+    public async Task<RefreshTokensResponseDto> Refresh([FromBody] RefreshTokensDto refreshTokensDto)
+    {
+        return await _authService.Refresh(refreshTokensDto);
     }
 }
